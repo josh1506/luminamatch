01522834283150fd15061827d630316a3da8f1f8
josh1506 <jmjabor@gmail.com>
2024-12-10 12:00:56 +0800

    [Bugfix] Fix profile stats section overlap issue on mobile responsiveness.

--- src/pages/ProfilePage/ProfileStatsSection.tsx
+++ src/pages/ProfilePage/ProfileStatsSection.tsx
@@ -8,7 +8,7 @@ const ProfileStatsSection: React.FC = () => {
                 <Button
                     key={index}
                     label={label}
-                    extraClassNames="font-bold text-white text-sm duration-300 bg-black bg-opacity-50 rounded border-none hover:bg-black hover:bg-opacity-70"
+                    extraClassNames="font-bold text-white text-sm duration-300 bg-black bg-opacity-50 rounded border-none hover:bg-black hover:bg-opacity-70 px-0.5 pt-0 pb-0 lg:py-1.5 lg:px-3"
                 />
             ))}
         </div>
