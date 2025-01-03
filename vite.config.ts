0f15c651a488a6d1b42c33d2d02202be095181f3
josh1506 <jmjabor@gmail.com>
2025-01-03 16:39:27 +0800

    [Bugfix] configure build output to /build folder and verify functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         },
     },
     plugins: [reactRouter(), tsconfigPaths()],
-    // build: {
-    //     outDir: 'build',
-    // },
+    build: {
+        outDir: 'build',
+    },
 });
