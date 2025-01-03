469e1fd9be98b91f46e67901daf543dbe9318bd1
josh1506 <jmjabor@gmail.com>
2025-01-03 16:17:58 +0800

    [Feature] Initialize React Router with basic setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
-    plugins: [react()],
-})
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+});
